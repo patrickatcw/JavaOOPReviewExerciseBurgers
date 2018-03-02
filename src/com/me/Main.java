ac7588b5dfe27647aa8f7abe628580c23bbcc3f3
patrickatcw <pj8392@yahoo.com>
2018-03-02 10:15:11 -0500

    initial structure, directions, and update gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,37 @@
+package com.me;
+//directions
+/*
+- the purpose of the application is to help Bills Burger's manage their process of selling hamburgers
+- application will help Bill to select types of burgers, some of the additional items (additions) to be added to the
+burgers and pricing
+- create a base burger, and 2 other types of burgers that are popular at the restaurant
+- the basic burger should have the following items;
+bread role type, meat, and up to 4 additional items (lettuce, tomato, pickle, etc) that the customers can select to be
+added to the burger
+- each one of the items gets charged an additional price so we need some way to track how many items got added and to
+calculate the final price (base burger with all the additions)
+- the burger has a base price and the additions are all priced separately (up to 4 items)
+- create a hamburger class to deal with all the above
+- the constructor should only include the roll type, meat and price, and can also include name of burger or you can use
+a setter
+- also create 2 extra varieties of hamburgers (subclasses);
+a) healthy burger (on a brown rye bread roll), plus 2 additional items that can be added
+- the healthy burger can have 6 items in total
+- hint: probably want to process the 2 additional items in the new class (subclass of hamburger), not the base class
+(Hamburger), since the 2 additions are only appropriate for this new class, in other words new burger type
+b) deluxe burger, comes with chips and drink as additions, but no extra additions are allowed
+hint; you have to find a way to automatically add these new additions at the time the deluxe burger object is created,
+and then prevent other additions being made
+- all 3 classes should have a method that can be called anytime to show the base price of the hamburger pus all
+additions, each showing the addition name, addition price, and a grand/final total for the burger
+(base price + all additions)
+- for the 2 additional classes this may require you to be looking at the base class for pricing and then adding totals
+to final price.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
